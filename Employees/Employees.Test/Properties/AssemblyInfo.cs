dd1935f00d7e1ea2ae23d6d13e63f71edfac6e4b
EVERECHEVERRI\Ever <everecheverri@gmail.com>
2018-09-29 22:15:26 -0500

    [Unit Tests Complete]

---
+++ Employees/Employees.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Employees.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Employees.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2c7ba285-55ea-4635-a185-6cdac6897427")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Employees/Employees.Test/UnitTestEmployees.cs
@@ -0,0 +1,89 @@
+﻿namespace Employees.Test
+{
+    using Employees.Business.Interfaces;
+    using Employees.Business.Services;
+    using Employees.Repository.Interfaces;
+    using Employees.Util.Dto;
+    using Employees.Util.Enums;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Moq;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+
+    [TestClass]
+    public class UnitTestEmployees
+    {
+        private readonly Mock<IEmployeesRepository> _mockEmployeesRepository = new Mock<IEmployeesRepository>();
+        private readonly IEmployeesService _employeesService;
+
+        public UnitTestEmployees()
+        {
+            _mockEmployeesRepository
+            .Setup(repo => repo.GetEmployeesAsync())
+            .ReturnsAsync((new List<EmployeeDto>() {
+                new EmployeeDto
+                {
+                    Id = 1,
+                    Name = "Mary",
+                    ContractTypeName = ContractTypeName.HourlySalaryEmployee,
+                    RoleId = 1,
+                    RoleName = "Administrator",
+                    RoleDescription = null,
+                    HourlySalary = 60000,
+                    MonthlySalary = 80000
+                },
+                new EmployeeDto
+                {
+                    Id = 2,
+                    Name = "Sam",
+                    ContractTypeName = ContractTypeName.HourlySalaryEmployee,
+                    RoleId = 1,
+                    RoleName = "Administrator",
+                    RoleDescription = null,
+                    HourlySalary = 60000,
+                    MonthlySalary = 80000
+                },
+                new EmployeeDto
+                {
+                    Id = 3,
+                    Name = "Erik",
+                    ContractTypeName = ContractTypeName.MonthlySalaryEmployee,
+                    RoleId = 2,
+                    RoleName = "Contractor",
+                    RoleDescription = null,
+                    HourlySalary = 60000,
+                    MonthlySalary = 80000
+                },
+                new EmployeeDto
+                {
+                    Id = 4,
+                    Name = "Jessica",
+                    ContractTypeName = ContractTypeName.MonthlySalaryEmployee,
+                    RoleId = 2,
+                    RoleName = "Contractor",
+                    RoleDescription = null,
+                    HourlySalary = 60000,
+                    MonthlySalary = 80000
+                },
+            }));
+
+            _employeesService = new EmployeesService(_mockEmployeesRepository.Object);
+        }
+
+        [TestMethod]
+        public async Task GetAllAsync()
+        {
+            var result = await _employeesService.GetEmployeesAsync();
+            Assert.IsTrue(result != null && result.Any());
+        }
+
+        [TestMethod]
+        public async Task GetByIdAsync()
+        {
+            var result = await _employeesService.GetEmployeeByIdAsync(1);
+            Assert.IsTrue(result != null && result.Any() && result.FirstOrDefault().Id == 1);
+        }
+    }
+}
